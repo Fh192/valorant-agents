ea7204e4eb91811969d8027be7858b28523f0e1d
Kirill Shevchenko <shevchenko.kirill192@gmail.com>
2022-04-12 01:19:43 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+});
+</script>
+
+<style lang="scss"></style>
