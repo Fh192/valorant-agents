41b4d71f784f029f31a46452ee4dba22f539984e
Kirill Shevchenko <shevchenko.kirill192@gmail.com>
2022-04-15 00:25:09 +0400

    Add AgentDetailsBiography

---
+++ src/components/AgentDetailsBiography.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="agentBiography">
+    <h3 class="label">// Biography</h3>
+    <p class="description">{{ biography }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AgentDetailsBiography',
+  props: { biography: { type: String, required: true } },
+});
+</script>
+
+<style lang="scss" scoped>
+.agentBiography {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  font-family: 'DINNextLTW04-Medium';
+  font-weight: 400;
+}
+
+.label {
+  text-transform: uppercase;
+}
+
+.description {
+  line-height: 1.5;
+}
+</style>
