e0c131cd0bc440c9716ed2b2128f315b5a056b58
Kirill Shevchenko <shevchenko.kirill192@gmail.com>
2022-04-15 00:27:56 +0400

    Add AgentFullPortrait

---
+++ src/components/AgentFullPortrait.vue
@@ -0,0 +1,23 @@
+<template>
+  <img class="agentFullPortrait" :src="portrait" alt="" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AgentFullPortrait',
+  props: { portrait: { type: String, required: true } },
+});
+</script>
+
+<style lang="scss" scoped>
+.agentFullPortrait {
+  height: 110%;
+  position: absolute;
+  top: 0;
+  right: 50%;
+  transform: translate(60%);
+  z-index: 10;
+}
+</style>
