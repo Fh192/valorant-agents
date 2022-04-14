c53cce0af5eea49d8167140fda14936e700d329d
Kirill Shevchenko <shevchenko.kirill192@gmail.com>
2022-04-15 00:36:41 +0400

    Add router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, RouteLocation } from 'vue-router';
+import AgentPage from '../pages/AgentPage.vue';
 import AgentsPage from '../pages/AgentsPage.vue';
 
 export const routes = [
   { path: '/', redirect: '/agents' },
   { path: '/agents', component: AgentsPage },
+  {
+    path: '/agents/:uuid',
+    component: AgentPage,
+    props: (route: RouteLocation) => ({ uuid: route.params.uuid }),
+  },
 ];
 
 export const router = createRouter({
