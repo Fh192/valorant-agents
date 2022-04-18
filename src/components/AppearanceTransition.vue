7a0e79a3e419b9c334f5838c8aa47e9ef3421cde
Kirill Shevchenko <shevchenko.kirill192@gmail.com>
2022-04-18 04:14:52 +0400

    Add AppearanceTransition

---
+++ src/components/AppearanceTransition.vue
@@ -0,0 +1,29 @@
+<template>
+  <transition name="appearance">
+    <slot />
+  </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppearanceTransition',
+});
+</script>
+
+<style lang="scss">
+.appearance-enter-active,
+.appearance-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+.appearance-enter-from {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.appearance-leave-to {
+  display: none;
+}
+</style>
